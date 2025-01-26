33d8c51cb0f99ee90bb9915513ecf12d3f4681da
Mohammed Ramadan <mohammedramadan2000@gmail.com>
2025-01-26 21:34:16 +0200

    hmm

--- src/drivers/driver.controller.ts
+++ src/drivers/driver.controller.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import driverService from './driver.service.js';
 import { StatusCodes } from 'http-status-codes';
 import { PaginatedResponse } from '../utils/types.js';
+import driverService from './driver.service.js';
 
 class DriverController {
 
-  findDrivers = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  findDrivers = async (req: Request, res: Response, next: NextFunction) => {
     const page = Number(req.query.page) || 1;
     const size = Number(req.query.size) || 10;
 
@@ -15,7 +15,7 @@ class DriverController {
         size,
       });
 
-      return res.status(StatusCodes.OK).json(new PaginatedResponse(
+      res.status(StatusCodes.OK).json(new PaginatedResponse(
         drivers,
         {
           currentPage,
@@ -24,17 +24,19 @@ class DriverController {
           totalPages,
         },
       ));
+      return;
     } catch (error) {
       next(error);
     }
   };
 
-  findDriverById = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  findDriverById = async (req: Request, res: Response, next: NextFunction) => {
     const { driverId } = req.params;
     try {
       const driver = await driverService.findDriverById(driverId);
 
-      return res.status(StatusCodes.OK).json({ driver });
+      res.status(StatusCodes.OK).json({ driver });
+      return;
     } catch (error) {
       next(error);
     }
